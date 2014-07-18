bfe6151b8a6504e15160615e910597ca7c352d22
Denis <denis.buybarov@blinkreaction.com>
2014-07-18 10:43:49 +0300

    add field_group module, custom module for visibility mobile banner functional

--- themes/custom/meningitis/scripts/scripts.js
+++ themes/custom/meningitis/scripts/scripts.js
@@ -1,5 +1,12 @@
 (function ($) {
   $(document).ready(function() {
+    // Change External link pop up settings
+    $.extend(Drupal.settings.extlink_extra.colorboxSettings, {
+      maxWidth: '745',
+      height: '374'
+    });
+
+
     $('#open-mobile-menu').bind('click', function () {
       $(this).toggleClass('opened');
       $(".pane-menu-menu-mobile-menu").slideToggle();
